e952571dbb0269ab2624451614233bdc471d0a26
YevheniiKostrych <156820347+YevheniiKostrych@users.noreply.github.com>
2024-06-30 23:42:19 +0200

    Update FizzBuzzThreadDigitsMapTest.java
    2

--- task2/FizzBuzzThreadDigitsMapTest.java
+++ task2/FizzBuzzThreadDigitsMapTest.java
@@ -3,28 +3,27 @@ package module12.task2;
 public class FizzBuzzThreadDigitsMapTest {
     public static void main(String[] args) {
         int n = 15;
-        FizzBuzzThreadDigitsMap fizzBuzzThreadDigitsMap = new FizzBuzzThreadDigitsMap(n);
-        for (int i = 1; i <= n; i++) {
-            final int current = i;
-            Thread threadA = new Thread(() -> fizzBuzzThreadDigitsMap.fizz(current));
-            Thread threadB = new Thread(() -> fizzBuzzThreadDigitsMap.buzz(current));
-            Thread threadC = new Thread(() -> fizzBuzzThreadDigitsMap.fizzbuzz(current));
-            Thread threadD = new Thread(() -> fizzBuzzThreadDigitsMap.number(current));
+        FizzBuzzThreadDigitsMap fizzBuzzThreadDigitsMap=new FizzBuzzThreadDigitsMap(n);
+            Thread threadA = new Thread(fizzBuzzThreadDigitsMap::fizz);
+            Thread threadB = new Thread(fizzBuzzThreadDigitsMap::buzz);
+            Thread threadC = new Thread(fizzBuzzThreadDigitsMap::fizzbuzz);
+            Thread threadD = new Thread(fizzBuzzThreadDigitsMap::number);
+            Thread printer = new Thread(fizzBuzzThreadDigitsMap::printOutput);
             threadA.start();
             threadB.start();
             threadC.start();
             threadD.start();
+            printer.start();
 
             try {
                 threadA.join();
                 threadB.join();
                 threadC.join();
                 threadD.join();
+                printer.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        fizzBuzzThreadDigitsMap.print();
-    }
 }
 
