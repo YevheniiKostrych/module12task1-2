513952c7b0c721279b65deb0cc96ab5ec16a216b
YevheniiKostrych <156820347+YevheniiKostrych@users.noreply.github.com>
2024-06-30 23:41:34 +0200

    Update FizzBuzzThreadDigitsMap.java
    2

--- task2/FizzBuzzThreadDigitsMap.java
+++ task2/FizzBuzzThreadDigitsMap.java
@@ -2,69 +2,95 @@ package module12.task2;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class FizzBuzzThreadDigitsMap {
     private final int n;
-    private final BlockingQueue<String> threadOueue = new LinkedBlockingQueue<>();
+    private final AtomicInteger current;
+    private final BlockingQueue<String> queue;
 
     public FizzBuzzThreadDigitsMap(int n) {
         this.n = n;
+        this.current = new AtomicInteger(1);
+        this.queue = new LinkedBlockingQueue<>();
     }
 
-    public void fizz(int i) {
-        if (i % 3 == 0 && i % 5 != 0) {
+    public void fizz() {
+        while (true) {
+            int num = current.get();
+            if (num > n) break;
+            if (num % 3 == 0 && num % 5 != 0) {
+                try {
+                    queue.put("fizz");
+                    current.incrementAndGet();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
+    public void buzz() {
+        while (true) {
+            int num = current.get();
+            if (num > n) break;
+            if (num % 5 == 0 && num % 3 != 0) {
+                try {
+                    queue.put("buzz");
+                    current.incrementAndGet();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
+    public void fizzbuzz() {
+        while (true) {
+            int num = current.get();
+            if (num > n) break;
+            if (num % 3 == 0 && num % 5 == 0) {
+                try {
+                    queue.put("fizzbuzz");
+                    current.incrementAndGet();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
+    public void number() {
+        while (true) {
+            int num = current.get();
+            if (num > n) break;
+            if (num % 3 != 0 && num % 5 != 0) {
+                try {
+                    queue.put(String.valueOf(num));
+                    current.incrementAndGet();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
+    public void printOutput() {
+        StringBuilder result = new StringBuilder();
+        while (true) {
             try {
-                threadOueue.put("fizz");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public void buzz(int i) {
-            if (i % 3 != 0 && i % 5 == 0) {
-                try {
-                    threadOueue.put("buzz");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-    }
-
-    public void fizzbuzz(int i) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                try {
-                    threadOueue.put("fizzbuzz");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-    }
-
-    public void number(int i) {
-            if (i % 3 != 0 && i % 5 != 0) {
-                try {
-                    threadOueue.put(String.valueOf(i));
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-        }
-    }
-
-
-    public void print() {
-        for (int i = 1; i <= n; i++) {
-            try {
-                System.out.print(threadOueue.take());
-                if (i < n) {
-                    System.out.print(", ");
-                } else {
-                    System.out.print(".");
+                String output = queue.take();
+                result.append(output).append(", ");
+                if (current.get() > n && queue.isEmpty()) {
+                    if (result.length() > 2) {
+                        result.setLength(result.length() - 2);
+                    }
+                    System.out.println(result.toString());
+                    break;
                 }
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                Thread.currentThread().interrupt();
             }
         }
     }
-
 }
